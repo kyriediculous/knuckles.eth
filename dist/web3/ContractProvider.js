e4f797755b2b84803389fcfa06da95373e014494
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-18 20:54:34 +0100

    v4

--- dist/web3/ContractProvider.js
+++ dist/web3/ContractProvider.js
@@ -8,18 +8,18 @@ exports.default = void 0;
 var _ethers = require("ethers");
 
 const ContractProvider = (artifact, walletOrProvider) => {
-  if (walletOrProvider.chainId) {
-    if (!artifact.networks[walletOrProvider.chainId]) {
+  if (walletOrProvider.network.chainId) {
+    if (!artifact.networks[walletOrProvider.network.chainId]) {
       throw new Error('Contract not deployed on provided network');
     }
 
-    return new _ethers.Contract(artifact.networks[walletOrProvider.chainId].address, artifact.abi, walletOrProvider);
+    return new _ethers.Contract(artifact.networks[walletOrProvider.network.chainId].address, artifact.abi, walletOrProvider);
   } else {
     if (!artifact.networks[walletOrProvider.provider.chainId]) {
       throw new Error('Contract not deployed on provided network');
     }
 
-    return new _ethers.Contract(artifact.networks[walletOrProvider.provider.chainId].address, artifact.abi, walletOrProvider);
+    return new _ethers.Contract(artifact.networks[walletOrProvider.provider.network.chainId].address, artifact.abi, walletOrProvider);
   }
 };
 
--- src/web3/ContractProvider.js
+++ src/web3/ContractProvider.js
@@ -1,16 +1,16 @@
 import {Contract} from 'ethers'
 
 const ContractProvider = (artifact, walletOrProvider) => {
-  if (walletOrProvider.chainId) {
-    if (!artifact.networks[walletOrProvider.chainId]) {
+  if (walletOrProvider.network.chainId) {
+    if (!artifact.networks[walletOrProvider.network.chainId]) {
       throw new Error('Contract not deployed on provided network')
     }
-    return new Contract(artifact.networks[walletOrProvider.chainId].address, artifact.abi, walletOrProvider)
+    return new Contract(artifact.networks[walletOrProvider.network.chainId].address, artifact.abi, walletOrProvider)
   } else {
     if (!artifact.networks[walletOrProvider.provider.chainId]) {
       throw new Error('Contract not deployed on provided network')
     }
-    return new Contract(artifact.networks[walletOrProvider.provider.chainId].address, artifact.abi, walletOrProvider)
+    return new Contract(artifact.networks[walletOrProvider.provider.network.chainId].address, artifact.abi, walletOrProvider)
   }
 }
 
