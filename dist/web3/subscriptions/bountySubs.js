f608f1925353ef181c201116a8f15a17a773c9e6
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-19 00:21:51 +0100

    v4

--- dist/web3/subscriptions/bountySubs.js
+++ dist/web3/subscriptions/bountySubs.js
@@ -3,11 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.onBountyCreated = void 0;
+exports.onBountyCommit = exports.onBountyAccepted = exports.onBountyCreated = void 0;
 
 var _utils = require("ethers/utils");
 
 var _BountyFactory = _interopRequireDefault(require("../artifacts/BountyFactory.json"));
+
+var _RecurringBountyFactory = _interopRequireDefault(require("../artifacts/RecurringBountyFactory.json"));
 
 var _Provider = _interopRequireDefault(require("../Provider"));
 
@@ -15,11 +17,51 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const onBountyCreated = (provider, callback) => {
   let event = new _utils.Interface(_BountyFactory.default.abi).events.logBountyCreated;
+  let recurringEvent = new _utils.Interface(_RecurringBountyFactory.default.abi).events.logRecurringBountyCreated;
   provider.on({
     topics: [event.topic]
   }, raw => {
     callback(event.decode(raw.data, raw.topics));
   });
+  provider.on({
+    topics: [recurringEvent.topic]
+  }, raw => {
+    callback(recurringEvent.decode(raw.data, raw.topics));
+  });
 };
 
 exports.onBountyCreated = onBountyCreated;
+
+const onBountyAccepted = (provider, callback) => {
+  let event = new _utils.Interface(_BountyFactory.default.abi).events.logBountyAccepted;
+  let recurringEvent = new _utils.Interface(_RecurringBountyFactory.default.abi).events.logRecurringBountyAccepted;
+  provider.on({
+    topics: [event.topic]
+  }, raw => {
+    callback(event.decode(raw.data, raw.topics));
+  });
+  provider.on({
+    topics: [recurringEvent.topic]
+  }, raw => {
+    callback(recurringEvent.decode(raw.data, raw.topics));
+  });
+};
+
+exports.onBountyAccepted = onBountyAccepted;
+
+const onBountyCommit = (provider, callback) => {
+  let event = new _utils.Interface(_BountyFactory.default.abi).events.logCommit;
+  let recurringEvent = new _utils.Interface(_RecurringBountyFactory.default.abi).events.logRecurringCommit;
+  provider.on({
+    topics: [event.topic]
+  }, raw => {
+    callback(event.decode(raw.data, raw.topics));
+  });
+  provider.on({
+    topics: [recurringEvent.topic]
+  }, raw => {
+    callback(recurringEvent.decode(raw.data, raw.topics));
+  });
+};
+
+exports.onBountyCommit = onBountyCommit;
--- src/web3/subscriptions/bountySubs.js
+++ src/web3/subscriptions/bountySubs.js
@@ -1,15 +1,56 @@
 import {Interface} from 'ethers/utils'
 
 import BountyFactory from '../artifacts/BountyFactory.json'
+import RecurringBountyFactory from '../artifacts/RecurringBountyFactory.json'
+
 import Provider from '../Provider'
 
 
 export const onBountyCreated = (provider, callback) => {
     let event = (new Interface(BountyFactory.abi)).events.logBountyCreated
-
+    let recurringEvent = (new Interface(RecurringBountyFactory.abi)).events.logRecurringBountyCreated
     provider.on({
       topics: [event.topic]
     }, raw => {
        callback(event.decode(raw.data, raw.topics))
     })
+
+    provider.on({
+      topics: [recurringEvent.topic]
+    }, raw => {
+      callback(recurringEvent.decode(raw.data, raw.topics))
+    })
+}
+
+
+export const onBountyAccepted = (provider, callback) => {
+  let event = (new Interface(BountyFactory.abi)).events.logBountyAccepted
+  let recurringEvent = (new Interface(RecurringBountyFactory.abi)).events.logRecurringBountyAccepted
+  provider.on({
+    topics: [event.topic]
+  }, raw => {
+     callback(event.decode(raw.data, raw.topics))
+  })
+
+  provider.on({
+    topics: [recurringEvent.topic]
+  }, raw => {
+    callback(recurringEvent.decode(raw.data, raw.topics))
+  })
+}
+
+export const onBountyCommit = (provider, callback) => {
+  let event = (new Interface(BountyFactory.abi)).events.logCommit
+  let recurringEvent = (new Interface(RecurringBountyFactory.abi)).events.logRecurringCommit
+  provider.on({
+    topics: [event.topic]
+  }, raw => {
+     callback(event.decode(raw.data, raw.topics))
+  })
+
+  provider.on({
+    topics: [recurringEvent.topic]
+  }, raw => {
+    callback(recurringEvent.decode(raw.data, raw.topics))
+  })
 }
