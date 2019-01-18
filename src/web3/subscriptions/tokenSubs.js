6e10092d1a044e7f15ceb399ffb3e8c9e102317d
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-18 14:45:02 +0100

    t

--- src/web3/subscriptions/tokenSubs.js
+++ src/web3/subscriptions/tokenSubs.js
@@ -1,11 +1,12 @@
-import {Interface} from 'ethers'
+import {Contract, Interface} from 'ethers'
 import Token from '../artifacts/Token.json'
 import Provider from '../Provider'
-
+import ContractProvider from '../ContractProvider'
 
 export const onTokenTransfer = (provider, callback) => {
-    let event = (new Interface(Token.abi)).events.Transfer
-
+    //let event = (new Interface(Token.abi)).events.Transfer
+    let token = new Contract(Token.networks[provider.chainId].address, Token.abi, provider)
+    //let token2 = ContractProvider(Token, provider)
     provider.on({
       topics: [event.topics[0]],
       address: Token.networks[provider.chainId].address
