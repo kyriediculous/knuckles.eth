254b11bec7f8afb4c7f01b982fc86c9e0ee7ff9e
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-20 00:04:23 +0100

    promises

--- dist/web3/subscriptions/tokenSubs.js
+++ dist/web3/subscriptions/tokenSubs.js
@@ -13,15 +13,26 @@ var _Provider = _interopRequireDefault(require("../Provider"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const onTokenTransfer = callback => {
+/*
+export const onTokenTransfer = callback => {
+    const provider = Provider(process.env.CLIENT)
+    let event = (new Interface(Token.abi)).events.Transfer
+    provider.on({
+      topics: [event.topic],
+      address: Token.networks[provider.network.chainId].address
+    }, raw => {
+       callback(event.decode(raw.data, raw.topics))
+    })
+}
+*/
+const onTokenTransfer = new Promise((resolve, reject) => {
   const provider = (0, _Provider.default)(process.env.CLIENT);
   let event = new _utils.Interface(_Token.default.abi).events.Transfer;
   provider.on({
     topics: [event.topic],
     address: _Token.default.networks[provider.network.chainId].address
   }, raw => {
-    callback(event.decode(raw.data, raw.topics));
+    if (raw !== undefined) resolve(raw);
   });
-};
-
+});
 exports.onTokenTransfer = onTokenTransfer;
--- src/web3/subscriptions/tokenSubs.js
+++ src/web3/subscriptions/tokenSubs.js
@@ -2,6 +2,7 @@ import {Interface} from 'ethers/utils'
 import Token from '../artifacts/Token.json'
 import Provider from '../Provider'
 
+/*
 export const onTokenTransfer = callback => {
     const provider = Provider(process.env.CLIENT)
     let event = (new Interface(Token.abi)).events.Transfer
@@ -12,3 +13,15 @@ export const onTokenTransfer = callback => {
        callback(event.decode(raw.data, raw.topics))
     })
 }
+*/
+
+export const onTokenTransfer = new Promise((resolve, reject) => {
+  const provider = Provider(process.env.CLIENT)
+  let event = (new Interface(Token.abi)).events.Transfer
+  provider.on({
+    topics: [event.topic],
+    address: Token.networks[provider.network.chainId].address
+  }, raw => {
+     if ( raw !== undefined ) resolve(raw);
+  })
+})
