88c623d209748aee74c821818616efadb0dd4272
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-21 21:59:20 +0100

    immutability

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,13 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.blockies = exports.utils = exports.services = exports.Web3 = exports.Wallet = void 0;
+Object.defineProperty(exports, "blockies", {
+  enumerable: true,
+  get: function () {
+    return _blockies.default;
+  }
+});
+exports.utils = exports.services = exports.Web3 = exports.Wallet = void 0;
 
 var Wallet = _interopRequireWildcard(require("./wallet/"));
 
@@ -21,8 +27,8 @@ var utils = _interopRequireWildcard(require("./utils/_"));
 
 exports.utils = utils;
 
-var blockies = _interopRequireWildcard(require("./utils/blockies"));
+var _blockies = _interopRequireDefault(require("./utils/blockies"));
 
-exports.blockies = blockies;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as Wallet from './wallet/'
 import * as Web3 from './web3/'
 import * as services from './services/'
 import * as utils from './utils/_'
-import * as blockies from './utils/blockies'
+import blockies from './utils/blockies'
 export {
   Wallet,
   Web3,
