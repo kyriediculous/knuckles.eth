388214b6926aa05fef8f22f8e22a69f9f1dfb96d
Nico Vergauwen <vergauwennico@gmail.com>
2019-01-29 09:39:00 +0100

    tokenSub options

--- src/web3/subscriptions/tokenSubs.js
+++ src/web3/subscriptions/tokenSubs.js
@@ -1,12 +1,14 @@
-import {Interface} from 'ethers/utils'
+import {Interface, hexlify, padZeros, arrayify} from 'ethers/utils'
 import Token from '../../../contracts/build/contracts/Token.json'
 import Provider from '../Provider'
 
-export const onTokenTransfer = callback =>  {
+export const onTokenTransfer = (to, callback) =>  {
   const provider = Provider(process.env.CLIENT)
   let event = (new Interface(Token.abi)).events.Transfer
+  let topics = [event.topic]
+  if (to) topics.push(hexlify(padZeros(arrayify(userAddress), 32)))
   provider.on({
-    topics: [event.topic],
+    topics: topics,
     address: Token.networks[provider.network.chainId].address
   }, raw => {
      if ( raw !== undefined ) callback(event.decode(raw.data, raw.topics));
